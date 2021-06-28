15a977f07c0ea723cccea140641aaf1b54d92af1
KilyongJeong <aceppin@motizensoft.com>
2021-06-28 14:48:12 +0900

    .

--- mulpang/routes/index.js
+++ mulpang/routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '멀팡' });
+});
+
+router.get('/*.html', function(req, res, next) {
+  var url = req.url.substring(1, req.url.indexOf('.html'));
+  res.render(url, { title: '오늘은 뭘파니?' });
 });
 
 module.exports = router;
