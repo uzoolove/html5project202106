4080be6c7d3a08bf3e84edf23d434345876aa8e4
KilyongJeong <aceppin@motizensoft.com>
2021-06-28 11:16:45 +0900

    .

--- nodeweb/webserver.js
+++ nodeweb/webserver.js
@@ -1,12 +1,24 @@
 var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
+var home = path.join(__dirname, 'html');
 
 var server = http.createServer(function(req, res){
   console.log(req.method, req.url, req.httpVersion);
   console.log(req.headers);
 
-  res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
-  res.end('<h1>Node 웹서버.</h1>');
+  var filename = req.url.substring(1);
+
+  try{
+    var data = fs.readFileSync(path.join(home, filename));
+    res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
+    res.end(data);
+  }catch(err){
+    res.writeHead(404, {'Content-Type': 'text/html;charset=utf-8'});
+    res.end('<h1>' + filename + ' 파일을 찾을 수 없습니다.');
+  }
+
 });
 
 
