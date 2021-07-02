1c99620530afaacfcaf8cba16f3f2a8235571942
KilyongJeong <aceppin@motizensoft.com>
2021-07-02 09:25:45 +0900

    .

--- mulpang/app.js
+++ mulpang/app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var session = require('express-session');
+var nocache = require('nocache');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -19,6 +20,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(nocache());
 
 // "/couponQuantity"로 시작하지 않는 url
 app.use(/^((?!\/couponQuantity).)*$/, session({
